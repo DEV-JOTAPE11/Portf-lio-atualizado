ad8d49d8697a1f1afa709539bde537e6cae339fa
joaop1109 <joaopedropiresqueiroz10@gmail.com>
2025-11-29 23:40:21 -0300

    Finalizando projeto com menu mobile

--- portofolio3/main.js
+++ portofolio3/main.js
@@ -145,3 +145,18 @@ document.addEventListener("DOMContentLoaded", () => {
     mainPage.classList.add("visible");
   }, 4000);
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".ul-list");
+
+if (hamburger && navMenu) {
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    });
+
+    document.querySelectorAll(".ul-list li a").forEach(n => n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }));
+}
